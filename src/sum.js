5feb629703c928901e6f8804688dfc719d1745a4
JulioViche <jeviche@espe.edu.ec>
2026-01-22 08:51:12 -0500

    feat: corrección

--- src/sum.js
+++ src/sum.js
@@ -1,7 +1,7 @@
 // Función que suma dos números
 // Recibe dos parámetros numéricos y retorna su suma
 function sum(a, b) {
-  return a + a;
+  return a + b;
 }
 
 // Exportar la función para su uso en otros archivos
